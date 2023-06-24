acc7ef7a4fb0ab3441c12bdac02a39d03341c3c1
AjnasNB <ajnasnb@gmail.com>
2023-06-24 23:26:29 +0530

    new

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(port);
+  await app.listen(process.env.PORT,'0.0.0.0');
 }
 bootstrap();
